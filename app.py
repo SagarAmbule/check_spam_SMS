afdd9889c514df657d4c0e11ee333181f091e04b
SAGAR AMBULE <sagarambule9@gmail.com>
2022-04-10 12:40:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("SMS Spam Classifier")
+st.image("https://media.istockphoto.com/vectors/spamming-concept-a-lot-of-emails-on-the-screen-of-a-monitor-email-box-vector-id902015458?k=20&m=902015458&s=612x612&w=0&h=7qoDgOL_q2-wA1rGG4G7EFAO6SQeL7j5bo9hqndDe78=")
+input_sms = st.text_area("Enter the message you want to check")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
